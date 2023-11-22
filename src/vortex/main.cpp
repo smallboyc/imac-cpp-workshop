ed39f8f93ff0e3086b5eeef56f0e7535c737bd69
Maxence Dupuis <104005395+smallboyc@users.noreply.github.com>
2023-11-22 14:45:58 +0100

    feat : vortex

--- src/vortex/main.cpp
+++ src/vortex/main.cpp
@@ -1,17 +1,28 @@
-#include <glm/gtx/matrix_transform_2d.hpp>
 #include <sil/sil.hpp>
+#include <glm/gtx/matrix_transform_2d.hpp>
+#include <cmath>
 
-glm::vec2 rotated(glm::vec2 v, float angle)
+glm::vec2 rotated(glm::vec2 point, glm::vec2 center_of_rotation, float angle)
 {
-    return glm::vec2{glm::rotate(glm::mat3{1.f}, angle) * glm::vec3{v, 1.f}};
+    return glm::vec2{glm::rotate(glm::mat3{1.f}, angle) * glm::vec3{point - center_of_rotation, 0.f}} + center_of_rotation;
 }
 
 int main()
 {
     sil::Image image{"images/logo.png"};
-    int x{0};
-    int y{0};
-    glm::vec2 position{x + image.width(), y + image.height()};
-    position = rotated(position, 12);
-    image.save("output/pouet.png");
+    sil::Image voidImage{image.width(), image.height()};
+
+    glm::vec2 positionCenter{image.width() / 2, image.height() / 2};
+    for (int x{0}; x < image.width(); x++)
+    {
+        for (int y{0}; y < image.height(); y++)
+        {
+            glm::vec2 point{x, y};
+            double rayon{sqrt(pow(x - positionCenter.x, 2) + pow(y - positionCenter.y, 2))};
+            glm::vec2 newPoint{rotated(point, positionCenter, rayon * 0.1f)};
+            if (newPoint.x < image.width() && newPoint.x >= 0 && newPoint.y < image.height() && newPoint.y >= 0)
+                voidImage.pixel(newPoint.x, newPoint.y) = image.pixel(x, y);
+        }
+    }
+    voidImage.save("output/pouet.png");
 }
