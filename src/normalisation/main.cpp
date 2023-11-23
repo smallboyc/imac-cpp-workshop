5944be6e29fe92bbcceb7b7fade81331bea7ead2
Maxence Dupuis <104005395+smallboyc@users.noreply.github.com>
2023-11-23 12:01:24 +0100

    feat : add normalisation

--- src/normalisation/main.cpp
+++ src/normalisation/main.cpp
@@ -1,9 +1,38 @@
 #include <sil/sil.hpp>
-#include <complex>
 
 int main()
 {
-    sil::Image image{500, 500};
+    sil::Image image{"images/photo_faible_contraste.jpg"};
+    float darkPixel{1.0f};
+    float whitePixel{0.0f};
+
+    for (int x{0}; x < image.width(); x++)
+    {
+        for (int y{0}; y < image.height(); y++)
+        {
+
+            float moyenne{(image.pixel(x, y).r + image.pixel(x, y).g + image.pixel(x, y).b) / 3.0f};
+
+            if (moyenne < darkPixel)
+            {
+                darkPixel = moyenne;
+            }
+            else if (moyenne > whitePixel)
+            {
+                whitePixel = moyenne;
+            }
+        }
+    }
+
+    for (int x{0}; x < image.width(); x++)
+    {
+        for (int y{0}; y < image.height(); y++)
+        {
+            image.pixel(x, y).r = (image.pixel(x, y).r - darkPixel) * 1 / whitePixel;
+            image.pixel(x, y).g = (image.pixel(x, y).g - darkPixel) * 1 / whitePixel;
+            image.pixel(x, y).b = (image.pixel(x, y).b - darkPixel) * 1 / whitePixel;
+        }
+    }
 
     image.save("output/pouet.png");
 }
